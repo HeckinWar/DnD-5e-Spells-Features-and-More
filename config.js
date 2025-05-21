fd27208001ca34e059f2ee712c34f8c7067cb026
HeckinWar <austincmcclurg@gmail.com>
2025-05-21 10:51:10 -0400

    Update 0.3.2
    Added more advancement options for features, mainly for features that grant traits or other features such as martial adept. Added source books to better keep track of references. Started working on backgrounds.

--- config.js
+++ config.js
@@ -6,3 +6,12 @@ Hooks.once("init", () => {
     CONFIG.DND5E.weaponTypeMap.firearm = "ranged"
     CONFIG.DND5E.weaponTypes.firearm = "Firearm"
 });
+//Allow feats to have more advancement types.
+//Mostly meant for general feats which grant things to players other than 
+//ability score improvements
+Hooks.on("init", () =>{
+    CONFIG.DND5E.advancementTypes.Trait.validItemTypes = new Set(["background", "class", "feat", "race", "subclass"])
+    CONFIG.DND5E.advancementTypes.ScaleValue.validItemTypes = new Set(["background", "class", "feat", "race", "subclass"])
+    CONFIG.DND5E.advancementTypes.ItemGrant.validItemTypes = new Set(["background", "class", "feat", "race", "subclass"])
+    CONFIG.DND5E.advancementTypes.ItemChoice.validItemTypes = new Set(["background", "class", "feat", "race", "subclass"])
+});
